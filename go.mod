799cc3fb87c9490c051e6aaab9c998aac1783b77
Gekko Wrld <gekkowrld@gmail.com>
2024-01-27 07:01:28 +0300

    style: Add styling and flags
    Colour capability is has been enabled.
    This can be disabled easily by passing `-c/--colour` flag.
    The colour is enabled by default unless there is no colour support in
    which case no colour will be displayed.
    256 colour display is required for any colour to be displayed, this is
        to ensure that even the most basic 8-bit display monitors can
        support the colour capabilities.
    The colours aren't meant to be a distraction, rather a more visually
    appealing way of displaying the program message.
    
    More flags have been added to fine tune the usage of the tool.
    This allows for the end user to dictate how the tool does its job
    instead of the other way round.
    The freedom should be given to the user not the tool.
    
    Signed-off-by: Gekko Wrld <gekkowrld@gmail.com>

--- go.mod
+++ go.mod
@@ -3,7 +3,13 @@ module github.com/gekkowrld/betty
 go 1.21.6
 
 require (
+	github.com/fatih/color v1.16.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jwalton/go-supportscolor v1.2.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/spf13/cobra v1.8.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/term v0.0.0-20210220032956-6a3ed077a48d // indirect
 )
--- src/output.go
+++ src/output.go
@@ -1,39 +1,48 @@
+
 package src
 
 import (
 	"fmt"
+	"github.com/fatih/color"
+	"github.com/jwalton/go-supportscolor"
 )
 
 type displayStr struct {
-  Main string
-  Extra string
+	Main  string
+	Extra string
 }
 
-func infoDisplay(msg displayStr) { 
-  if FlagsPassed.Verbose {
-    fmt.Printf("\nINFO:\n%s\n%s\n", msg.Main, msg.Extra)
-  }else if FlagsPassed.Quiet {
-    // Display Nothing
-  }else {
-    fmt.Printf("INFO: %s\n", msg.Main)
+func canSupportColour() bool {
+	// Return false if the user has turned it off (and default)
+	return !flagsPassed.Colour && supportscolor.Stdout().SupportsColor && supportscolor.Stdout().Has256
+}
+
+func displayMessage(prefix string, msg displayStr, colorFunc func(format string, a ...interface{})) {
+	if flagsPassed.Verbose {
+		if canSupportColour() {
+			colorFunc("\n%s: %s\n%s\n", prefix, msg.Main, msg.Extra)
+		} else {
+			fmt.Printf("\n%s: %s\n%s\n", prefix, msg.Main, msg.Extra)
+		}
+	}
+  if !flagsPassed.Verbose && !flagsPassed.Quiet {
+		if canSupportColour() {
+			colorFunc("\n%s: %s\n", prefix, msg.Main)
+		} else {
+			fmt.Printf("\n%s: %s\n", prefix, msg.Main)
+		}
   }
 }
 
-func errorDisplay(msg displayStr)  {
-  if FlagsPassed.Verbose {
-    fmt.Printf("\nERROR:\n%s\n%s\n", msg.Main, msg.Extra)
-  }else if FlagsPassed.Quiet {
-    // Display Nothing
-  }else {
-    fmt.Printf("ERROR: %s\n", msg.Main)
-  }
+func infoDisplay(msg displayStr) {
+	displayMessage("INFO", msg, color.HiYellow)
 }
 
-func successDisplay(msg displayStr)  {
-  if FlagsPassed.Verbose {
-    fmt.Printf("\nSuccess:\n%s\n%s\n", msg.Main, msg.Extra)
-  }else if FlagsPassed.Quiet {
-    // Display Nothing
-  }else {
-    fmt.Printf("Success: %s\n", msg.Main)
-  }}
+func errorDisplay(msg displayStr) {
+	displayMessage("ERROR", msg, color.HiRed)
+}
+
+func successDisplay(msg displayStr) {
+	displayMessage("Success", msg, color.HiGreen)
+}
+
